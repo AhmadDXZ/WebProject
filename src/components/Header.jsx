7607076154450b20f1017c237d765e5fedee6ae5
Ahmad Abdullah <ahmadhisham133@gmail.com>
2025-12-30 00:22:53 +0300

    Adding files for the server and mongodb connectivity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   return (
     <header className="header">
       <nav className="nav">
-        <div className="left-nav">
-          <h1 className="logo">Online Bookshop</h1>
-          <div className="nav-links">
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/about">About</Link>
-          </div>
+        <Link to="/" className="logo">Online Bookshop</Link>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/about">About</Link>
         </div>
       </nav>
     </header>
